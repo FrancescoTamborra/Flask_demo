9d2364dea2c2c4305ae620d34636903e33676127
Francesco <francesco@francescos-mac-mini.home>
2021-03-29 11:22:31 +0200

    basic templates with Jinja

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #print(__name__)
 app = Flask(__name__) # we run it as main, see below
@@ -8,7 +8,7 @@ app = Flask(__name__) # we run it as main, see below
 @app.route('/') # we use a decorator for the next function applied to the route /
 @app.route('/index') # we can apply the same function index to multiple routes
 def index():
-    return 'Yo !'
+    return render_template('index.html', current_title='Custom Title') # Flask has Jinja tamplate engine which allows to render much more than plain html (dynamic)
 
 if __name__ == '__main__':
     app.run(debug=True) # always use degug in order to have useful info from Flask
