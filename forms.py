c99f07440b570e0008b9ca0dc3dd3331c3fd18f1
Francesco <francesco@francescos-mac-mini.home>
2021-03-29 17:32:20 +0200

    final working version with delete and edit.
    TODO: requirements and db script ?

--- forms.py
+++ forms.py
@@ -6,3 +6,6 @@ from wtforms.validators import DataRequired # many validators, here we just want
 class AddTaskForm(FlaskForm): # we create our class which inherits from FlaskForm
     title = StringField('Title', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class DeleteTaskForm(FlaskForm):
+    submit = SubmitField('Delete') # we pass the task and we can show the name but we don't need a field for that
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from datetime import datetime
 
 import models
@@ -18,5 +18,40 @@ def add():
         t = models.Task(title=form.title.data, date=datetime.utcnow())
         db.session.add(t)
         db.session.commit()
+        flash('Task added to the database')
         return redirect(url_for('index')) # if success we redirect on index where all tasks are shown
     return render_template('add.html', form=form) # in the render we call it form
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id): # we now pass the task id used in the urls and in SQL queries
+    task = models.Task.query.get(task_id)
+    form = forms.AddTaskForm()
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash('Task has been updated')
+            return redirect(url_for('index'))
+
+        form.title.data = task.title # already filled with actual title
+        return render_template('edit.html', form=form, task_id=task_id)
+    else:
+        flash('Task not found')
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
+def delete(task_id):
+    task = models.Task.query.get(task_id)
+    form = forms.DeleteTaskForm()
+    if task:
+        if form.validate_on_submit():
+            db.session.delete(task)
+            db.session.commit()
+            flash('Task has been deleted')
+            return redirect(url_for('index'))
+
+        return render_template('delete.html', form=form, task_id=task_id, title=task.title)
+    else:
+        flash('Task not found')
+    return redirect(url_for('index'))
